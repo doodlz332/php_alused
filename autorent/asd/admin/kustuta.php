07ad73c863bcd5ed4df52064b20bc6b36c2f9d36
doodlz332 <emillaur1999@gmail.com>
2026-03-19 12:12:36 +0200

    asd
    asd

---
+++ autorent/asd/admin/kustuta.php
@@ -0,0 +1,20 @@
+<!------DELETE FROM CARS WHERE ID =46 ----->
+<?php include('../config.php'); ?>
+<?php
+        if(!empty($_GET['delid'])) 
+            $id = $_GET['delid'];
+      $paring = "DELETE FROM cars WHERE id=$id";
+     $valjund = mysqli_query($yhendus, $paring);
+    if ($valjund) {
+        echo"Kustutatud"; 
+        header("Location: index.php");      
+       } else {
+            echo "Urror";
+       }
+    
+
+
+
+
+
+ ?>
--- autorent/asd/header.php
+++ autorent/asd/header.php
@@ -1,14 +1,14 @@
 <!doctype html>
-<html lang="et">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Autorent</title>
+    <title>'Autorent'</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <!-- menüü -->
-      <nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
+    <!-- /menüü -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
   <div class="container">
     <a class="navbar-brand" href="index.php">Autorent</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,18 +17,18 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" href="index.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-      </ul>
+          </ul>
       <form class="d-flex" role="search" method="get" action="index.php">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="otsi">
         <button class="btn btn-outline-success" type="submit">Otsi</button>
@@ -36,4 +36,4 @@
     </div>
   </div>
 </nav>
-    <!-- /menüü -->
+ <!-- /menüü -->
